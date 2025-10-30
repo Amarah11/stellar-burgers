1126736aff76b2fcb399ad8eed50ac0b34b13643
Aliev Amrah <aar2103@yandex.ru>
2025-10-31 00:10:40 +0300

    fixed-typing-errors

--- src/components/ui/burger-constructor/type.ts
+++ src/components/ui/burger-constructor/type.ts
@@ -1,7 +1,12 @@
-import { TOrder } from '@utils-types';
+import { TConstructorIngredient, TOrder } from '@utils-types';
+
+export type TConstructorItems = {
+  bun: TConstructorIngredient | null; // булочка может отсутствовать
+  ingredients: TConstructorIngredient[]; // массив ингредиентов
+};
 
 export type BurgerConstructorUIProps = {
-  constructorItems: any;
+  constructorItems: TConstructorItems;
   orderRequest: boolean;
   price: number;
   orderModalData: TOrder | null;
